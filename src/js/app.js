ba4a65e4d274a696b48bfb3b16b62cce57e2d088
Vital Ozierski <vital@ozerich.com>
2020-05-20 21:55:47 +0300

    finalize design

--- src/js/app.js
+++ src/js/app.js
@@ -15,9 +15,14 @@ const validators = {
 };
 
 function getCommonErrorLabel(code) {
-    switch (code) {
-        case 400 < code < 500 : return "Invalid Server Response";
-        case 499 < code : return "Server Internal Error";
+    if (code === 404) {
+        return 'Server not found'
+    } else if (code >= 400 && code < 500) {
+        return "Invalid Server Response"
+    } else if (code >= 500) {
+        return "Server Internal Error"
+    } else {
+        return "Unknown Error";
     }
 }
 
@@ -46,25 +51,25 @@ class LoginForm {
         this.loader.classList.remove('show');
     }
 
-     submit() {
-         const values = this.getFormValues();
-         const data = {
-             grant_type: "password",
-             client_id: "1",
-             password: values.password,
-             username: values.login,
-             provider: "administrators",
-         }
+    submit() {
+        const values = this.getFormValues();
+        const data = {
+            grant_type: "password",
+            client_id: "1",
+            password: values.password,
+            username: values.login,
+            provider: "administrators",
+        }
 
-         this.addSubmitting();
-         fetch('/api/oauth/token', {
+        this.addSubmitting();
+        fetch('/api/oauth/token', {
             method: 'POST',
             body: JSON.stringify(data),
-             mode: "cors",
-             headers: {
+            mode: "cors",
+            headers: {
                 "Accept": "application/json",
                 "Content-Type": "application/json"
-             }
+            }
         }).then((response) => {
             if (response.ok) {
                 this.commonError.textContent = '';
@@ -79,25 +84,25 @@ class LoginForm {
 
             this.commonError.textContent = getCommonErrorLabel(response.status);
             throw new Error(getCommonErrorLabel(response.status));
-         }).then(result => {
-             if (isValidBody(result)) {
-                 localStorage.setItem('accessToken', result.access_token);
-                 localStorage.setItem('refreshToken', result.refresh_token);
-                 window.location.href = '/admin'
-             } else {
-                 const error = 'Invalid authentication response';
-                 this.commonError.textContent = error;
-                 throw new Error(error);
-             }
-         }).catch((error) => {
-             if (error instanceof Error) {
-                 if (error.toString().includes('Failed to fetch')) {
-                     this.commonError.textContent = 'Service is not available';
-                 }
-             }
-         }).finally(() => {
-             this.removeSubmitting();
-         });
+        }).then(result => {
+            if (isValidBody(result)) {
+                localStorage.setItem('accessToken', result.access_token);
+                localStorage.setItem('refreshToken', result.refresh_token);
+                window.location.href = '/admin'
+            } else {
+                const error = 'Invalid authentication response';
+                this.commonError.textContent = error;
+                throw new Error(error);
+            }
+        }).catch((error) => {
+            if (error instanceof Error) {
+                if (error.toString().includes('Failed to fetch')) {
+                    this.commonError.textContent = 'Service is not available';
+                }
+            }
+        }).finally(() => {
+            this.removeSubmitting();
+        });
     }
 
     validate() {
@@ -116,7 +121,7 @@ class LoginForm {
     }
 
     getInputCollection() {
-        return  this.form.querySelectorAll('input');
+        return this.form.querySelectorAll('input');
     }
 
     getFormValues() {
@@ -129,7 +134,7 @@ class LoginForm {
         return values;
     }
 
-    syncErrors(errors){
+    syncErrors(errors) {
         this.getInputCollection().forEach(input => {
             input.nextElementSibling.textContent = errors[input.name] || '';
         })
