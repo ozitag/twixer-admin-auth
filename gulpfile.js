aed798186d60883fb8c1fc5147b12d2ffbb6964c
Vital Ozierski <v.ozierski@ozitag.com>
2020-06-06 10:34:32 +0300

    change logo configuration

--- gulpfile.js
+++ gulpfile.js
@@ -15,97 +15,110 @@ const fs = require("fs");
 sass.compiler = require("node-sass");
 
 gulp.task("sass", () => {
-  return gulp
-    .src("./src/sass/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(rename("app.css"))
-    .pipe(cleanCSS({ compatibility: "ie9" }))
-    .pipe(gulp.dest("./build"));
+    return gulp
+        .src("./src/sass/**/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(rename("app.css"))
+        .pipe(cleanCSS({compatibility: "ie9"}))
+        .pipe(gulp.dest("./build"));
 });
 
 gulp.task("html", function () {
-  const templateData = {
-    title: process.env.PAGE_TITLE || "OZiTAG",
-    base_path: process.env.BASE_PATH || "",
-    is_logo_png: process.env.IS_LOGO_PNG && (process.env.IS_LOGO_PNG.toString() === '1' || process.env.IS_LOGO_PNG.toString().toUpperCase() === 'TRUE'),
-    brand_color: process.env.BRAND_COLOR || "#DD6900",
-    language: process.env.LANGUAGE || "EN",
-  };
 
-  const options = {
-    ignorePartials: true,
-    batch: [`src`],
-    helpers: {
-      times: function (n, block) {
-        let accum = "";
-        for (let i = 0; i < n; ++i) accum += block.fn(i + 1);
-        return accum;
-      },
-      ifCond: function (v1, v2, options) {
-        if (v1 === v2) {
-          return options.fn(this);
+    const envLogo = process.env.LOGO;
+    const basePath = process.env.BASE_PATH || "";
+
+    let logo = null;
+    if ('LOGO' in process.env) {
+        if (typeof process.env.LOGO === "string" && process.env.LOGO.length > 0 && process.env.LOGO.toLowerCase() !== 'false' && process.env.LOGO.toLowerCase() !== 'null' && process.env.LOGO !== '0') {
+            logo = envLogo.startsWith('http:') ? envLogo : basePath + envLogo;
         }
-        return options.inverse(this);
-      },
-      concat: function (...args) {
-        return `${args.slice(0, -1).join("")}`;
-      },
-    },
-  };
+    } else {
+        logo = "logo.svg";
+    }
 
-  return gulp
-    .src("src/index.hbs")
-    .pipe(handlebars(templateData, options))
-    .pipe(rename("index.html"))
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest("build"));
+    const templateData = {
+        title: process.env.PAGE_TITLE || "OZiTAG",
+        base_path: basePath,
+        logo: logo ? logo : null,
+        brand_color: process.env.BRAND_COLOR || "#DD6900",
+        language: process.env.LANGUAGE || "EN",
+    };
+
+    const options = {
+        ignorePartials: true,
+        batch: [`src`],
+        helpers: {
+            times: function (n, block) {
+                let accum = "";
+                for (let i = 0; i < n; ++i) accum += block.fn(i + 1);
+                return accum;
+            },
+            ifCond: function (v1, v2, options) {
+                if (v1 === v2) {
+                    return options.fn(this);
+                }
+                return options.inverse(this);
+            },
+            concat: function (...args) {
+                return `${args.slice(0, -1).join("")}`;
+            },
+        },
+    };
+
+    return gulp
+        .src("src/index.hbs")
+        .pipe(handlebars(templateData, options))
+        .pipe(rename("index.html"))
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest("build"));
 });
 
 gulp.task("scripts", () => {
-  return gulp
-    .src("src/js/app.js")
-    .pipe(
-      babel({
-        presets: [
-          [
-            "@babel/preset-env",
-            {
-              useBuiltIns: "entry", // alternative mode: "entry"
-              corejs: 3, // default would be 2
-              targets: "> 0.25%, not dead",
-              // set your own target environment here (see Browserslist)
-            },
-          ],
-        ],
-      })
-    )
-    .pipe(rename("app.js"))
-    .pipe(uglify())
-    .pipe(gulp.dest("build"));
+    return gulp
+        .src("src/js/app.js")
+        .pipe(
+            babel({
+                presets: [
+                    [
+                        "@babel/preset-env",
+                        {
+                            useBuiltIns: "entry", // alternative mode: "entry"
+                            corejs: 3, // default would be 2
+                            targets: "> 0.25%, not dead",
+                            // set your own target environment here (see Browserslist)
+                        },
+                    ],
+                ],
+            })
+        )
+        .pipe(rename("app.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("build"));
 });
 
 gulp.task("watch", () => {
-  watch("src/*.hbs", gulp.series("html"));
-  watch("src/sass/**/*.scss", gulp.series("sass"));
-  watch("src/js/**/*.js", gulp.series("scripts"));
+    watch("src/*.hbs", gulp.series("html"));
+    watch("src/sass/**/*.scss", gulp.series("sass"));
+    watch("src/js/**/*.js", gulp.series("scripts"));
 });
 
 gulp.task("favicon", () => {
-  return gulp.src("./assets/favicon/*").pipe(gulp.dest("./build/favicon"));
+    return gulp.src("./assets/favicon/*").pipe(gulp.dest("./build/favicon"));
 });
 
 gulp.task("logo", () => {
-  const files = [];
+    const files = [];
 
-  if (fs.existsSync("./assets/logo.png")) {
-    files.push("./assets/logo.png");
-  }
+    if (fs.existsSync("./assets/logo.png")) {
+        files.push("./assets/logo.png");
+    }
 
-  if (fs.existsSync("./assets/logo.svg")) {
-    files.push("./assets/logo.svg");
-  }
+    if (fs.existsSync("./assets/logo.svg")) {
+        files.push("./assets/logo.svg");
+    }
 
-  return gulp.src(files).pipe(gulp.dest("./build"));
+    return gulp.src(files).pipe(gulp.dest("./build"));
 });
 
 gulp.task("build", gulp.parallel("favicon", "logo", "html", "sass", "scripts"));
--- scripts/develop.sh
+++ scripts/develop.sh
@@ -1,7 +1,7 @@
 npm install -g yarn
 yarn install
-docker build -t ozitag/tvixer-admin-auth .
-docker stop tvixer-admin-auth
-docker run --rm -d -p 3000:80 --name=tvixer-admin-auth -v=$(pwd)/build:/usr/share/nginx/html ozitag/tvixer-admin-auth
+docker build -t ozitag/tager-admin-auth .
+docker stop tager-admin-auth
+docker run --rm -d -p 3000:80 --name=tager-admin-auth -v=$(pwd)/build:/usr/share/nginx/html ozitag/tager-admin-auth
 open http://localhost:3000
 gulp watch
